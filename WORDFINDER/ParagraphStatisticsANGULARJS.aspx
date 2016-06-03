8e5de47c15acfbfa8c05f732b751feccae62f1d2
Eric Dell'Oro <eric.delloro@gmail.com>
2016-06-03 12:41:41 +1000

    CODE UPDATES
    CODE UPDATES

--- WORDFINDER/ParagraphStatisticsANGULARJS.aspx
+++ WORDFINDER/ParagraphStatisticsANGULARJS.aspx
@@ -9,72 +9,8 @@
  
     <script src="Scripts/angular.js"></script>
 
-    <script>
-
-    var app = angular.module("myApp", []);
-
-    app.controller('myController', function ($scope, $http) {
-
-    $scope.textFilter = "ZORK ADVENTURE";
-    $scope.textResponse = [];          
-    $scope.myFunct = function (keyEvent) {
-    var myrequest = $scope.textRequest;
-    var myfilter = $scope.textFilter;
-
-    $http({
-        method: 'POST',
-        url: "/WebServiceStatistics.asmx/GetTextStatistics",
-        dataType: 'json',
-        data: { 'paragraph': escape(myrequest) , 'filter' : escape(myfilter) },
-        contentType: 'application/json; charset=utf-8'
-    }).success(function (response) {
-        var data = response.d;
-        $scope.textResponse = data;
-        $scope.$apply();
-                   
-    }).error(function (data, status, headers, config) {
-        //$scope.textResponse = "ERROR";
-    });        
-    } //KEY EVENT
-
-    $scope.CallWebServiceInit = function () {
-               
-        $http({
-            method:      'POST',
-            url: '/WebServiceStatistics.asmx/GetZorkText',
-            dataType: 'json',
-            data: {}, //SEND JSON RETURN JSON
-            //data:   {name:'TEST123'}             
-            contentType: 'application/json; charset=utf-8' 
-        }).success(function (response) {
-            var data = response.d;
-            $scope.textRequest = data.payload;
-                 
-            $http({
-                method: 'POST',
-                url: "/WebServiceStatistics.asmx/GetTextStatistics",
-                dataType: 'json',
-                data: { 'paragraph': escape(data.payload), filter: escape($scope.textFilter) },
-                contentType: 'application/json; charset=utf-8'
-            }).success(function (response) {
-
-                var data = response.d;
-                      
-                $scope.textResponse = data;
-                    
-            }).error(function (data, status, headers, config) {
-                alert('error');
-                //$scope.textResponse = "ERROR";
-            });
-
-        }).error(function (data, status, headers, config) {  }); //$http
-    }; // CALL WEB SERVICE
-
-    $scope.CallWebServiceInit();
-
-}); //CONTROLLER
-
-</script>
+    <script src="Scripts/code-angular.js"></script>
+   
 </head>
 <body>
 <form id="textstatisticsANGULAR" runat="server" ng-controller="myController">
---
+++ WORDFINDER/Scripts/code-jquery.js
@@ -0,0 +1,92 @@
+﻿$(document).ready(function () {
+
+    initWebRequest();
+
+    $('#txFilter,#txRequest').keyup(function () {
+        var text = $('#txRequest').val();
+        var filter = $('#txFilter').val();
+        text = text.replace(/(\r\n|\n|\r)/gm, " ");
+        filter = filter.replace(/(\r\n|\n|\r)/gm, " ");
+        text = text.replace(/'/g, "\\'");
+        callWebRequest(text, filter);
+    });
+});
+
+
+
+
+function initWebRequest() {
+
+  
+        var urlRequest = "WebServiceStatistics.asmx/GetZorkText";
+
+        $.ajax({
+            url: urlRequest,
+            type: "POST",
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            data: "{}",
+            success: function (result) {
+                var data = result.d;             
+                $('#txRequest').text(data.payload);
+                var starttext = $('#txRequest').text();
+                var startfilter = $('#txFilter').text();
+                starttext = starttext.replace(/(\r\n|\n|\r)/gm, " ");
+                starttext = starttext.replace(/'/g, "\\'");
+                startfilter = startfilter.replace(/(\r\n|\n|\r)/gm, " ");
+                callWebRequest(starttext, startfilter);
+            },
+
+            error: function (xhr, status, error) {
+                alert('error');
+                var err = eval("(" + xhr.responseText + ")");
+                $("#divResult").html(err.Message)
+            }
+        });
+ 
+
+
+
+
+}
+
+function callWebRequest(requestdata, filterdata) {
+
+    var urlRequest = "WebServiceStatistics.asmx/GetTextStatistics";
+
+    $.ajax({
+        url: urlRequest,
+        type: "POST",
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+
+        data: "{" +
+              "  paragraph : '" + requestdata + "' , " +
+              "  filter    : '" + filterdata + "'  " +
+              "}",
+
+        success: function (result) {
+            var data = result.d;
+            var text = makeTable(data);
+            $("#divResultInner").html(text);
+            $("#divResultInner").css('width', '100%');
+        },
+
+        error: function (xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            $("#divResult").html(err.Message)
+        }
+    });
+}
+
+function makeTable(obj) {
+    var html = '<table>';
+    for (var i = 0; i < obj.length ; i++) {
+        html += '<tr>';
+        html += '<td>' + obj[i].key + '</td>';
+        html += '<td>' + obj[i].payload + '</td>';
+        html += '</tr>';
+    }
+    html += '</table>';
+    return html;
+}
